664c97fb594b0c6eea2505e769206b2f9067ff88
Munirabonu <muniratillaxojayeva@gmail.com>
2023-01-25 14:48:41 +0500

    navbar text

---
+++ index.js
@@ -0,0 +1,17 @@
+// git init
+// git add .
+// git commit -m"first"
+// git branch -M main
+// git remove http.....
+// git push -u origin main
+
+
+// git branch test
+// git branch
+// git checkout test
+
+// code ozgartirish
+
+// git add . 
+// git commit -m"test uchun coment"
+// git push -u origin test
